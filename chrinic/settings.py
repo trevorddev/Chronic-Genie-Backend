291cebd4c6090827fec0797400bd10e2f7d3a14c
trevorddev <trvrdrmmnd@gmail.com>
2021-09-02 02:20:34 +0500

    added mysql conf

--- chrinic/settings.py
+++ chrinic/settings.py
@@ -94,23 +94,23 @@ WSGI_APPLICATION = 'chrinic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'chronic_genie',
-#         'USER': 'db_client',
-#         'PASSWORD': 'ez$1r|ns$MGZKaT',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chronic_genie',
+        'USER': 'db_client',
+        'PASSWORD': 'ez$1r|ns$MGZKaT',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
