c3a359a95cccc528bd9b6f17cd3a30a0e39b62ab
trevorddev <trvrdrmmnd@gmail.com>
2021-09-12 00:04:29 +0500

    attr added

--- user/models.py
+++ user/models.py
@@ -54,6 +54,7 @@ class Account(AbstractBaseUser):
     is_active				= models.BooleanField(default=True)
     is_staff				= models.BooleanField(default=False)
     is_superuser			= models.BooleanField(default=False)
+    is_email_subscribed		= models.BooleanField(default=True)
 
 
     USERNAME_FIELD = 'email'
