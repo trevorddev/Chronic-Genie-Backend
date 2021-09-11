b7bc93407bd2782fa61f547c24c55f77eca9b770
trevorddev <trvrdrmmnd@gmail.com>
2021-09-12 00:05:27 +0500

    email func completed

--- user/api/utils.py
+++ user/api/utils.py
@@ -1,22 +1,31 @@
-from django.core.mail import EmailMessage
+# from django.core.mail import EmailMessage
 
 
-import threading
+# import threading
 
 
-class EmailThread(threading.Thread):
+# class EmailThread(threading.Thread):
 
-    def __init__(self, email):
-        self.email = email
-        threading.Thread.__init__(self)
+#     def __init__(self, email):
+#         self.email = email
+#         threading.Thread.__init__(self)
 
-    def run(self):
-        self.email.send()
+#     def run(self):
+#         self.email.send()
 
+
+# class Util:
+#     @staticmethod
+#     def send_email(data):
+#         email = EmailMessage(
+#             subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
+#         EmailThread(email).start()
+
+
+from django.core.mail import send_mail
+from django.conf import settings
 
 class Util:
     @staticmethod
     def send_email(data):
-        email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-        EmailThread(email).start()
+        send_mail(data['email_subject'], data['email_body'], settings.FROM_EMAIL_ADDRESS, [data['to_email']])
