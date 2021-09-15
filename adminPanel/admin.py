28e461fef169776da2f4782ed7d525628d1d5a16
trevorddev <trvrdrmmnd@gmail.com>
2021-09-15 11:50:50 +0500

    export option for marketing email

--- adminPanel/admin.py
+++ adminPanel/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+import csv
+from django.http import HttpResponse
+
 from adminPanel.models import (
     AppSettings,
     Food,
@@ -19,6 +22,32 @@ class AppSettingsAdmin(admin.ModelAdmin):
 		else:
 			return True
 
+@admin.action(description='Export to CSV')
+def export_as_csv(modeladmin, request, queryset):
+
+	meta = modeladmin.model._meta
+	field_names = [field.name for field in meta.fields]
+
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+	writer = csv.writer(response)
+
+	
+	writer.writerow(["Email", "is_subscribed"])
+	for s in queryset:
+		writer.writerow([s.email, s.is_subscribed])
+	return response
+class MarketingEmailAdmin(admin.ModelAdmin):
+	list_display = ('email', 'is_subscribed')
+	search_fields = ('email',)
+
+	filter_horizontal = ()
+	list_filter = ('is_subscribed',)
+	fieldsets = ()
+	ordering = ('email',)
+
+	actions = [export_as_csv]
+
 admin.site.register(AppSettings, AppSettingsAdmin)
 admin.site.register(Food)
 admin.site.register(Aggravator)
@@ -26,4 +55,4 @@ admin.site.register(Symptom)
 admin.site.register(Comorbidity)
 admin.site.register(DailyMedication)
 admin.site.register(FlareMedication)
-admin.site.register(MarketingEmail)
+admin.site.register(MarketingEmail, MarketingEmailAdmin)
