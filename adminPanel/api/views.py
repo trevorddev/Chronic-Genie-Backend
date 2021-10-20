5d352c8f43916c810e870d4a5b5a38eac5fdbf0d
trevorddev <trvrdrmmnd@gmail.com>
2021-10-21 01:43:00 +0500

    undo last commit

--- adminPanel/api/views.py
+++ adminPanel/api/views.py
@@ -24,12 +24,12 @@ from .serializers import(
 )
 
 
-@permission_classes((IsAuthenticated))
+@permission_classes((IsAuthenticated, IsAdminUser))
 class FoodAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
 
-@permission_classes((IsAuthenticated))
+@permission_classes((IsAuthenticated, IsAdminUser))
 class FoodCreateRetrieve(generics.ListCreateAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
